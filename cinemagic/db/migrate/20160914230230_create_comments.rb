1e7523335c6beaa9db242bd11d02cffa23180e61
Camille Villa <cvilla100@gmail.com>
2016-09-14 16:22:18 -0700

    Add text field to comments migration

--- cinemagic/db/migrate/20160914230230_create_comments.rb
+++ cinemagic/db/migrate/20160914230230_create_comments.rb
@@ -1,7 +1,8 @@
 class CreateComments < ActiveRecord::Migration[5.0]
   def change
     create_table :comments_tables do |t|
-      t.references, :review
+      t.text :text
+      t.references :review
       t.references :user, foreign_key: true
     end
   end
