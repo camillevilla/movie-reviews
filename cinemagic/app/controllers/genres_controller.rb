ae6019e6e35cf52b6d0100dacf5717a37774bf73
Camille Villa <cvilla100@gmail.com>
2016-09-15 12:19:40 -0700

    Add Genres controller

---
+++ cinemagic/app/controllers/genres_controller.rb
@@ -0,0 +1,36 @@
+class GenresController < ApplicationController
+  def index
+    @genres = Genre.all
+  end
+
+  def new
+    @genre = Genre.new
+  end
+
+  def create
+    @genre = Genre.new(genre_params)
+
+    if @genre.save
+      redirect_to genre_path(@genre)
+    else
+      render :new
+    end
+  end
+
+  def show
+    @genre = Genre.find(params[:id])
+  end
+
+  def destroy
+    @genre = Genre.find(params[:id])
+
+    @genre.destroy
+    redirect_to genres_path
+  end
+
+  private
+
+  def genre_params
+    params.require(:genre).permit(:title, :year, :director)
+  end
+end
