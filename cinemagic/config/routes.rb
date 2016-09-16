5ed7d98fd1f1e3adb2e22b2c6332ef131288c19d
Blake Wills <blake.wills525@gmail.com>
2016-09-16 11:46:29 -0700

    Create search engine for movie api

--- cinemagic/config/routes.rb
+++ cinemagic/config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'moviedb/index'
+
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :movies do
@@ -6,6 +8,8 @@ Rails.application.routes.draw do
   end
 
   resources :genres
+
+  get 'movies/search'
 
   get 'sessions/new'
 
